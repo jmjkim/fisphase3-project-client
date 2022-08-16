abf196ca544196dcb189eb77fa84b65a9f7e04eb
JK <jmjk@JKs-MacBook-Pro.local>
2022-08-15 17:04:05 -0700

    Changes fetch URL

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, {useState} from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+ 
 import DepartmentSelector from "./components/DepartmentSelector";
 import EngineDisplayer from "./components/EngineDisplayer";
+
+import DepartmentForm from "./components/DepartmentForm";
 import CreateEngineForm from "./components/CreateEngineForm";
 import UpdateEngineForm from "./components/UpdateEngineForm";
 
 const App = () => {
   document.title = "EMNF-S"
 
-  const storedDepartment = sessionStorage.getItem("storedDepartment")
+  const sessionStoredDepartment = sessionStorage.getItem("sessionStoredDepartment")
+  const [storedDepartment, setStoredDepartment] = useState(sessionStoredDepartment)
 
   return (
-    <React.Fragment>
+    <div>
+      <h1 className="app_title">Engine Manufacturing Status Manager</h1>
+
       <Router>
-        <DepartmentSelector/>
+        <DepartmentSelector setStoredDepartment={setStoredDepartment}/>
         
         <Routes>
-          <Route path={`/engine_department/engines/${storedDepartment}`} exact element={<EngineDisplayer storedDepartment={storedDepartment}/>}/> 
-          <Route path={`/engine_department/engines/${storedDepartment}/create`} exact element={<CreateEngineForm storedDepartment={storedDepartment}/>}/>
-          <Route path={`/engine_department/engines/${storedDepartment}/update`} exact element={<UpdateEngineForm storedDepartment={storedDepartment}/>}/>
+          <Route path={`/engines/${storedDepartment}`} exact element={<EngineDisplayer storedDepartment={storedDepartment}/>}/> 
+          <Route path={`/create`} exact element={<DepartmentForm storedDepartment={storedDepartment}/>}/>
+          <Route path={`/engines/${storedDepartment}/create`} exact element={<CreateEngineForm storedDepartment={storedDepartment}/>}/>
+          <Route path={`/engines/${storedDepartment}/update`} exact element={<UpdateEngineForm storedDepartment={storedDepartment}/>}/>
         </Routes>
       </Router>
-    </React.Fragment>
+    </div>
   );
 }
 
--- src/components/EngineDisplayer.js
+++ src/components/EngineDisplayer.js
@@ -1,14 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 import Navbar from "./Navbar"
 
 const EngineDisplayer = ({storedDepartment}) => {
-    const navigateTo = useNavigate()
-
     const [engines, setEngines] = useState([])
 
     useEffect(() => {
-        fetch(`http://localhost:9292/engine_department/engines/${storedDepartment}`)
+        fetch(`http://localhost:9292/engines/${storedDepartment}`)
         .then(r => r.json())
         .then(setEngines)
         .catch(err => alert(err.message))
@@ -22,53 +21,52 @@ const EngineDisplayer = ({storedDepartment}) => {
                 {Object.values(engines).map((engine, index) => {
                     return (
                         <React.Fragment key={engine.id}>
-                            <div className="list_of_engine_row" onClick={() => {
-                                    sessionStorage.setItem("storedEngineObjKeys", Object.keys(engine))
-                                    sessionStorage.setItem("storedEngineObjValues", Object.values(engine))
+                            <Link to={`/engines/${storedDepartment}/update`} className="link">
+                                <div className="list_of_engine_row" onClick={() => {
+                                        sessionStorage.setItem("storedEngineObjKeys", Object.keys(engine))
+                                        sessionStorage.setItem("storedEngineObjValues", Object.values(engine))
+                                    }}>
 
-                                    navigateTo("update")
-                                }}>
+                                    <div className="number_of_engine_container">
+                                        <b>Engine Number #{index + 1}</b>
+                                    </div>
 
-                                <div className="number_of_engine_container">
-                                    <b>#{index + 1}</b>
+                                    <div className="list_of_engine_non_part_container">
+                                        <div><b>Engine ID:</b> <span id="eid">{engine.manufactured_engine_id}</span></div>
+                                        <div><b>VIN:</b> {engine.associated_vehicle_vin}</div>
+                                        <div><b>Layout:</b> {engine.engine_layout}</div>
+                                        <div><b>Remark:</b> {engine.remark}</div>
+                                    </div>
+
+                                    <div className="status_indicator">
+                                        <div className="completed_status">Green: Complete</div>
+                                        <div className="incompleted_status">Red: Incomplete</div>
+                                    </div>
+
+                                    <div className="list_of_engine_part_dotted_container"> 
+                                        {engine.camshaft_built ? <div className="completed_status">camshaft_built</div> :
+                                                                 <div className="incompleted_status">camshaft_built</div>}
+
+                                        {engine.piston_built ? <div className="completed_status">piston_built</div> :
+                                                               <div className="incompleted_status">piston_built</div>}
+
+                                        {engine.flywheel_built ? <div className="completed_status">flywheel_built</div> :
+                                                                 <div className="incompleted_status">flywheel_built</div>}
+
+                                        {engine.connecting_rod_built ? <div className="completed_status">connecting_rod_built</div> :
+                                                                       <div className="incompleted_status">connecting_rod_built</div>}
+
+                                        {engine.crankshaft_built ? <div className="completed_status">crankshaft_built</div> :
+                                                                   <div className="incompleted_status">crankshaft_built</div>}
+
+                                        {engine.sump_built ? <div className="completed_status">sump_built</div> :
+                                                             <div className="incompleted_status">sump_built</div>}
+
+                                        {engine.camshaft_drive_belt_built ? <div className="completed_status">camshaft_drive_belt_built</div> :
+                                                                            <div className="incompleted_status">camshaft_drive_belt_built</div>}
+                                    </div>
                                 </div>
-
-                                <div className="list_of_engine_non_part_container">
-                                    <div><b>Engine Size:</b> {engine.engine_size}</div>
-                                    <div><b>Engine ID:</b> <span id="manufactured_engine_id">{engine.manufactured_engine_id}</span></div>
-                                    <div><b>VIN:</b> {engine.associated_vehicle_vin}</div>
-                                    <div><b>Layout:</b> {engine.engine_layout}</div>
-                                    <div><b>Remark:</b> {engine.remark}</div>
-                                </div>
-
-                                <div className="status_indicator">
-                                    <div className="completed_status">Green: Complete</div>
-                                    <div className="incompleted_status">Red: Incomplete</div>
-                                </div>
-
-                                <div className="list_of_engine_part_dotted_container"> 
-                                    {engine.camshaft_built ? <div className="completed_status">camshaft_built</div> :
-                                                             <div className="incompleted_status">camshaft_built</div>}
-
-                                    {engine.piston_built ? <div className="completed_status">piston_built</div> :
-                                                           <div className="incompleted_status">piston_built</div>}
-
-                                    {engine.flywheel_built ? <div className="completed_status">flywheel_built</div> :
-                                                             <div className="incompleted_status">flywheel_built</div>}
-
-                                    {engine.connecting_rod_built ? <div className="completed_status">connecting_rod_built</div> :
-                                                                   <div className="incompleted_status">connecting_rod_built</div>}
-
-                                    {engine.crankshaft_built ? <div className="completed_status">crankshaft_built</div> :
-                                                               <div className="incompleted_status">crankshaft_built</div>}
-
-                                    {engine.sump_built ? <div className="completed_status">sump_built</div> :
-                                                         <div className="incompleted_status">sump_built</div>}
-
-                                    {engine.camshaft_drive_belt_built ? <div className="completed_status">camshaft_drive_belt_built</div> :
-                                                                        <div className="incompleted_status">camshaft_drive_belt_built</div>}
-                                </div>
-                            </div>
+                            </Link>
                         </React.Fragment>
                     )})}
             </div>
